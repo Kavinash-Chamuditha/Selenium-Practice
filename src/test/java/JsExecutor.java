9a9ad525291c6e243313f303da4ab865380fa434
Oshana Kavinash 0743563623 <oshanachamuditha@gmail.com>
2026-04-18 15:38:03 +0530

    haddle elements and scroll page

--- src/test/java/JsExecutor.java
+++ src/test/java/JsExecutor.java
@@ -6,26 +6,60 @@ import org.testng.annotations.Test;
 public class JsExecutor {
 
     WebDriver driver;
+    JavascriptExecutor jsExecutor;
 
     @BeforeMethod
-    public void OpenTestPage(){
+    public void OpenTestPage() {
         driver = new ChromeDriver();
         driver.manage().window().maximize();
         driver.get("https://testautomationpractice.blogspot.com/");
+        jsExecutor = (JavascriptExecutor) driver;
     }
 
     //get alert box
-@Test
-    public void jsexecutorTest(){
-    JavascriptExecutor jsExecutor= (JavascriptExecutor) driver;
+    @Test
+    public void jsexecutorTest() throws InterruptedException {
+
 
 //    jsExecutor.executeScript("alert('Welcome');");
 
-    //set a input value in a textbox
-    WebElement inputNameTextBox=driver.findElement(By.xpath("//*[@id=\"name\"]"));
-    jsExecutor.executeScript("arguments[0].value='Kavinash';",inputNameTextBox);
+        //set a input value in a textbox
+        WebElement inputNameTextBox = driver.findElement(By.xpath("//*[@id=\"name\"]"));
+        jsExecutor.executeScript("arguments[0].value='Kavinash';", inputNameTextBox);
+
+        Thread.sleep(3000);
+
+        //Highlight element
+        jsExecutor.executeScript("arguments[0].style.border='3px solid red'", inputNameTextBox);
+        jsExecutor.executeScript("arguments[0].style.background='yellow'", inputNameTextBox);
+
+        Thread.sleep(3000);
+        //click Element
+        WebElement malecheckBox = driver.findElement(By.xpath("(//input[@id='male'])"));
+        jsExecutor.executeScript("arguments[0].click()", malecheckBox);
+
+        Thread.sleep(3000);
+
 
     }
 
+    @Test
+    //scrolling page
+    public void scrollPage() throws InterruptedException {
+        jsExecutor.executeScript("window.scrollTo(0,1000)");
 
+        Thread.sleep(3000);
+        jsExecutor.executeScript("window.scrollTo(0,0)");
+
+        Thread.sleep(3000);
+        WebElement scrollElement=driver.findElement(By.xpath("//*[@id=\"post-body-1307673142697428135\"]/div[3]/label"));
+        jsExecutor.executeScript("arguments[0].scrollIntoView(true)", scrollElement);
+    }
 }
+
+
+
+
+
+
+
